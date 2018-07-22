fa9a9b9e5f6547f6f1a1cfadca95dda0f8eec442
Aditya choudhary <adityachoudhary54@gmail.com>
2018-07-22 17:26:32 +0530

    add 'nice to have' msg
    Signed-off-by: Aditya choudhary <adityachoudhary54@gmail.com>

--- file05.js
+++ file05.js
@@ -4,5 +4,6 @@ function greet(name){
     console.log("Knock Knock Knock "+name)
     console.log("Knock Knock Knock "+name)
     console.log("Knock Knock Knock "+name)
+    console.log("Nice to have u here")
 }
 greet('Aditya choudhary')
